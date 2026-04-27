091a98422c880a58d5379907aa2f83c6b48a0170
Marx Segovia <itsmemarx.im@gmail.com>
2026-04-27 08:20:11 +0800

    Background title component

---
+++ BackgroundTitle.jsx
@@ -0,0 +1,14 @@
+function BackgroundTitle() {
+    return (
+        <div className="background-title">
+            <h1 className="content">
+                <strong>
+                    <span className="uep">UEP </span>
+                    <span className="credly"> CREDLY</span>
+                </strong>
+            </h1>
+        </div>
+    );
+}
+
+export default BackgroundTitle
