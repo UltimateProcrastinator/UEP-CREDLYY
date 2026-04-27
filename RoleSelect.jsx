b1baaf8fc6f0f2c2c6b8ec82230f818de071b7f6
Marx Segovia <itsmemarx.im@gmail.com>
2026-04-27 21:54:18 +0800

    Component for role select for user rehistration

---
+++ RoleSelect.jsx
@@ -0,0 +1,33 @@
+function RoleSelect({ role, setRole }) {
+    return (
+        <div className="form-group">
+            <label>Role</label>
+
+            <select
+                value={role}
+                onChange={(e) =>
+                    setRole(e.target.value)
+                }
+            >
+                <option value="">
+                    Select Role
+                </option>
+
+                <option value="student">
+                    Student
+                </option>
+
+                <option value="alumni">
+                    Alumni
+                </option>
+
+                <option value="staff">
+                    Staff
+                </option>
+
+            </select>
+        </div>
+    );
+}
+
+export default RoleSelect;
